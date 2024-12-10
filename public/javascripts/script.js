ed41d6b3365759b7488fed79a228d453c5e18cde
Brandon Lee <brandovlee@10-248-123-69.wireless.oregonstate.edu>
2024-12-09 19:14:30 -0800

    Added support for dislike button.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Array to store liked items
     const likedItems = [];
     const likeButton = document.querySelector(".heart-btn");
+    const dislikeButton = document.querySelector(".cross-btn");
 
     // Add event listener to like button
     likeButton.addEventListener("click", () => {
@@ -22,10 +23,36 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         // Only add item if it is not already liked
-        const isLiked = likedItems.some((item) => item.name === likedItem.name);
+        const isLiked = likedItems.some(
+            (item) =>
+                item.name === itemName &&
+                item.description === itemDescription &&
+                item.image === itemImage
+        );
         if (!isLiked) {
             // Add item to likedItems array
             likedItems.push(likedItem);
         }
+    });
+
+    // Add event listener to dislike button
+    dislikeButton.addEventListener("click", () => {
+        // Get item card
+        const itemCard = dislikeButton.closest(".item-card");
+
+        // Get item details
+        const itemName = itemCard.querySelector("#item-name").textContent;
+        const itemDescription =
+            itemCard.querySelector("#item-description").textContent;
+        const itemImage = itemCard.querySelector("#item-image").src;
+
+        // Remove item from likedItems array
+        const itemIndex = likedItems.findIndex(
+            (item) =>
+                item.name === itemName &&
+                item.description === itemDescription &&
+                item.image === itemImage
+        );
+        likedItems.splice(itemIndex, 1);
     });
 });
